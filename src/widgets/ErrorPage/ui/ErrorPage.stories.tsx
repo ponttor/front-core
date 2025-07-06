a5e1b841015e1cb3d713a1f409c53e635f08fb3f
Alexander Illarionov <ruzinovrun@gmail.com>
2025-07-06 18:11:48 +0200

    add storybook

--- src/widgets/ErrorPage/ui/ErrorPage.stories.tsx
+++ src/widgets/ErrorPage/ui/ErrorPage.stories.tsx
@@ -1,24 +1,23 @@
-// import React from 'react';
-// import { ComponentStory, Meta } from '@storybook/react';
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-// import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
-// import { Theme } from 'app/providers/ThemeProvider';
-// import { ErrorPage } from './ErrorPage';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ErrorPage } from './ErrorPage';
 
-// export default {
-//   title: 'widget/ErrorPage',
-//   component: ErrorPage,
-//   argTypes: {
-//     backgroundColor: { control: 'color' },
-//   },
-// } as Meta<typeof ErrorPage>;
+export default {
+  title: 'widget/ErrorPage',
+  component: ErrorPage,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof ErrorPage>;
 
-// const Template: ComponentStory<typeof ErrorPage> = (args) => <ErrorPage {...args} />;
+const Template: ComponentStory<typeof ErrorPage> = args => <ErrorPage {...args} />;
 
-// export const Light = Template.bind({});
-// Light.args = {};
+export const Light = Template.bind({});
+Light.args = {};
 
-// export const Dark = Template.bind({});
-// Dark.args = {};
-// Dark.decorators = [ThemeDecorator(Theme.DARK)];
-export default {};
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
